734a18e0d2adbb717699176456bab8d57bcaa5d3
Bhavya-1201 <bhavyaainala46@gmail.com>
2026-01-31 09:32:46 +0530

    Create DBConnection.java

---
+++ DBConnection.java
@@ -0,0 +1,16 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+public class DBConnection {
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/student_db",
+                "root",
+                "password"
+            );
+        } catch (Exception e) {
+            System.out.println("Database connection failed!");
+            return null;
+        }
+    }
+}
